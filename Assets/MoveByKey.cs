dc2e54d4ba86a116b5019617806f2d1f42b57c38
xuyenlon87 <xuyenlon87@gmail.com>
2023-06-12 21:19:46 +0700

    21h20 12/6

---
+++ Assets/MoveByKey.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveByKey : MonoBehaviour
+{
+    public CharacterController characterController;
+    public float movingSpeed;
+
+    private void OnValidate()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        Vector3 direction = transform.right * hInput + transform.forward * vInput;
+        characterController.SimpleMove(direction * movingSpeed);
+
+    }
+}
