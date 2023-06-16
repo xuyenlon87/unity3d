e5cd19767977f0d1edab211b31693a9b288dbce8
xuyenlon87 <xuyenlon87@gmail.com>
2023-06-16 21:29:16 +0700

    22h 16/6

--- Assets/RotateByMouse.cs
+++ Assets/RotateByMouse.cs
@@ -1,14 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class RotateByMouse : MonoBehaviour
 {
     public float anglePerSecond;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
 
@@ -17,10 +13,10 @@ public class RotateByMouse : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
-        float yaw = mouseX * anglePerSecond * Time.deltaTime ;
+        float yaw = mouseX * anglePerSecond * Time.deltaTime;
         transform.Rotate(0, yaw, 0);
         float yaw1 = mouseY * anglePerSecond * Time.deltaTime;
         transform.Rotate(-yaw1, 0, 0);
-        
+
     }
 }
