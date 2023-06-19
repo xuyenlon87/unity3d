da24dad75510cd85e18f037a5a3ead2260f413e5
xuyenlon87 <xuyenlon87@gmail.com>
2023-06-19 21:18:20 +0700

    21h 19/6

--- Assets/MoveByKey.cs
+++ Assets/MoveByKey.cs
@@ -6,10 +6,34 @@ public class MoveByKey : MonoBehaviour
 {
     public CharacterController characterController;
     public float movingSpeed;
-
+    public float jumpHeight;
+    public bool dkJump;
     private void OnValidate()
     {
         characterController = GetComponent<CharacterController>();
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            dkJump = true;
+        }
+    }
+    private void OnMove()
+    {
+        float jInput = Input.GetAxis("Jump");
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = transform.right * hInput + transform.forward * vInput;
+        characterController.SimpleMove(direction * movingSpeed);
+        if (dkJump)
+        {
+            if (jInput > 0)
+            {
+                transform.position = new Vector3(transform.position.x, jumpHeight + movingSpeed * Time.deltaTime, transform.position.z);
+            }
+        }
     }
     // Start is called before the first frame update
     void Start()
@@ -20,11 +44,8 @@ public class MoveByKey : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
-        Vector3 direction = transform.right * hInput + transform.forward * vInput;
-        characterController.SimpleMove(direction * movingSpeed);
 
+        OnMove();
 
     }
 }
--- Assets/RotateByMouse.cs
+++ Assets/RotateByMouse.cs
@@ -1,22 +1,43 @@
+using UnityEngine;
+
 public class RotateByMouse : MonoBehaviour
 {
     public float anglePerSecond;
+    public float angleOverDistance;
+    public Transform cameraHolder;
+    public float minPitch;
+    public float maxPitch;
+    public float pitch;
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-
-    // Update is called once per frame
-    void Update()
+    void UpdateYaw()
     {
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
         float yaw = mouseX * anglePerSecond * Time.deltaTime;
         transform.Rotate(0, yaw, 0);
-        float yaw1 = mouseY * anglePerSecond * Time.deltaTime;
-        transform.Rotate(-yaw1, 0, 0);
+        //float yaw1 = mouseY * anglePerSecond * Time.deltaTime;
+        //transform.Rotate(-yaw1, 0, 0);
+    }
+    void UpdatePitch()
+    {
+        float mouseY = Input.GetAxis("Mouse Y");
+        float deltaPitch = -mouseY * angleOverDistance;
+        pitch = Mathf.Clamp(pitch + deltaPitch, minPitch, maxPitch);
+        cameraHolder.localEulerAngles = new Vector3(pitch, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateYaw();
+        UpdatePitch();
+
 
     }
 }
